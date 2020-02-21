ba3a5fc5dbacb4a4f54eba7ed75a9b46e1ce65eb
yyf <2381510397@qq.com>
2020-02-21 11:42:55 +0800

    zxcvbn

--- MyJspProject/src/JDBCDemo.java
+++ MyJspProject/src/JDBCDemo.java
@@ -3,7 +3,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
+//sdsfcs
 public class JDBCDemo {
 	private static final String URL = "jdbc:mysql://localhost:3306/test1";
 	private static final String USERNAME = "root";
